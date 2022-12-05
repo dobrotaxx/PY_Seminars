98e437e03baf7d1a815989da208ba2671c684087
Semyon <tonkih.sem@mail.ru>
2022-12-05 17:47:31 +0500

    added task1 from seminar3

---
+++ Урок 3. Данные, функции и модули в Python/1. Перемешивание списка.py
@@ -0,0 +1,14 @@
+# 1. Реализуйте алгоритм перемешивания списка.
+import time
+
+def rand_num(min=0, max=10):
+    num = int((time.time() % 1) * (max - min) + min)
+    return num
+
+lst = [2, -10, 5, 8, 43]
+print(lst)
+
+for i in range(len(lst)):
+    j = rand_num(0, len(lst) - 1)
+    lst[i], lst[j] = lst[j], lst[i]
+print(lst)
