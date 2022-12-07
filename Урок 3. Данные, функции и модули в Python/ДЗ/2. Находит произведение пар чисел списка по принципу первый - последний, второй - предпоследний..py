0afe8eb06217af7950633b16cf956f7802fb6089
Semyon <tonkih.sem@mail.ru>
2022-12-07 21:20:18 +0500

    added DZ02 for Seminar03

---
+++ Урок 3. Данные, функции и модули в Python/ДЗ/2. Находит произведение пар чисел списка по принципу первый - последний, второй - предпоследний..py
@@ -0,0 +1,23 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример:
+#     - [2, 3, 4, 5, 6] => [12, 15, 16];
+#     - [2, 3, 5, 6] => [12, 15]
+import math
+
+def NewList(userList):
+    lengthOfNewList = math.ceil(len(userList) / 2)
+    finallyList = []
+    for i in range(lengthOfNewList):
+        if i != (len(userList) - i - 1):
+            newList = userList[i] * userList[(len(userList) - i) - 1]
+            finallyList.append(newList)
+        else:
+            newList *= userList[i]
+            finallyList.append(newList)
+    print(finallyList)
+
+
+userList = list(map(int, input('Задайте список (через пробел, без запятых): ').split()))
+print(userList)
+NewList(userList)
