b924ecc8559efde3e7963fc14502bfd9448446e7
Semyon <tonkih.sem@mail.ru>
2022-12-08 13:19:24 +0500

    added DZ05 for Seminar03

---
+++ Урок 3. Данные, функции и модули в Python/ДЗ/5. Негафибоначчи.py
@@ -0,0 +1,22 @@
+# 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример:
+#     - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+userNum = int(input('Введите число: '))
+
+
+def GetNegofibonacci(userNum):
+    fiboNums = []
+    a, b = 1, 1
+    for i in range(userNum):
+        fiboNums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range(userNum + 1):
+        fiboNums.insert(0, a)
+        a, b = b, a - b
+    return fiboNums
+
+
+fiboNums = GetNegofibonacci(userNum)
+print(GetNegofibonacci(userNum))
