f991e3325c7a185340d749103386a01c43c1fc99
Semyon <tonkih.sem@mail.ru>
2022-12-13 01:03:34 +0500

    added DZ04 for Seminar04

---
+++ Урок 4. Данные, функции и модули в Python. Продолжение/ДЗ/4. Формирует рандомный список коэф многочлена и записывает в файл многочлен заданной степени.py
@@ -0,0 +1,19 @@
+# 4. Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100)* многочлена и
+# записать в файл многочлен степени k.
+#     *Пример:*
+#     - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+#     -  k=5 => 2*x^5 + 4*x^4 + 2*x^3 + 2*x^2 + 4*x + 5 = 0
+from random import randint
+
+k = 3
+coefficients = [randint(0, 100) for i in range(0, k)]
+result = []
+
+j = 0
+while k != 0:
+    result.extend((f'{coefficients[j]}*x^{k}', ' ', '+', ' '))
+    j += 1
+    k -= 1
+result.append(f'{randint(0, 100)} = 0')
+print(''.join(result))
