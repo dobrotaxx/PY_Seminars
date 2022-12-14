19948d22c295b931c1ced5df5eb06baaa7c010ef
Semyon <tonkih.sem@mail.ru>
2022-12-14 18:03:18 +0500

    added DZ05 for Seminar04

--- Урок 4. Данные, функции и модули в Python. Продолжение/ДЗ/5. Считает сумму многочленов из двух разных файлов..py
+++ Урок 4. Данные, функции и модули в Python. Продолжение/ДЗ/5. Считает сумму многочленов из двух разных файлов..py
@@ -3,49 +3,67 @@
 with open('file1.txt', 'r') as f1, open('file2.txt', 'r') as f2:
     line1 = f1.readline()
     line2 = f2.readline()
-    print(f' {line1}\n',f'{line2}\n')
+    print(f' {line1}\n', f'{line2}\n')
 
 lst = line1.split(' = ')[0]
+print(lst)
 lst2 = line2.split(' = ')[0]
+print(lst2)
+lst = lst.split(' + ')
+lst2 = lst2.split(' + ')
+print(lst)
+print(lst2)
 
-lst = lst.split('x^')
-lst = ''.join(lst)
-lst = lst.split()
+dict1 = {}
+dict2 = {}
 
-lst2 = lst2.split('x^')
-lst2 = ''.join(lst2)
-lst2 = lst2.split()
+for i in range(len(lst)):
+    if lst[i].count('x^'):
+        dict1[int(lst[i].split('^')[1])] = int(lst[i].split('*')[0])
+    elif lst[i].count('x'):
+        dict1[1] = int(lst[i].split('*')[0])
+    else:
+        dict1[0] = int(lst[i])
+print(dict1)
 
+for i in range(len(lst2)):
+    if lst2[i].count('x^'):
+        dict2[int(lst2[i].split('^')[1])] = int(lst2[i].split('*')[0])
+    elif lst2[i].count('x'):
+        dict2[1] = int(lst2[i].split('*')[0])
+    else:
+        dict2[0] = int(lst2[i])
+print(dict2)
 
-lst3 = []
-lst4 = []
-for i in lst:
-    if i.isdigit():
-        lst3.append(i)
+max_index = 0
+for key in dict1.keys():
+    if max_index < key:
+        max_index = key
 
-lst3 = list(map(int, lst3))
+for key in dict2.keys():
+    if max_index < key:
+        max_index = key
 
-for i in lst2:
-    if i.isdigit():
-        lst4.append(i)
-
-lst4 = list(map(int, lst4))
+dict3 = {}
+for i in range(max_index, -1, -1):
+    if i in dict1:
+        if i in dict2:
+            dict3[i] = dict1[i] + dict2[i]
+        else:
+            dict3[i] = dict1
+    elif i in dict2:
+        dict3[i] = dict2
+print(dict3)
 
 result = []
-for i in range(0, len(lst3)):
-    if i % 2 == 0:
-        if i == len(lst3)-1:
-            result.append(f'{lst3[i] + lst4[i]}')
+for i in range(max_index, -1, -1):
+    if i in dict3:
+        if i == 1:
+            result.append(str(dict3[i]) + ' * x')
+        elif i == 0:
+            result.append(str(dict3[i]))
         else:
-            result.append(f'{lst3[i] + lst4[i]} * x^')
-    else:
-        if i == len(lst3)-1:
-            result.append(f'{lst3[i] + lst4[i]}')
-        else:
-            result.append(f'{lst3[i] + lst4[i]} + ')
-result.append(' = 0')
-result = ''.join(result)
-print(result)
+            result.append(str(dict3[i]) + ' * x^' + str(i))
 
 with open('file3.txt', 'w') as data:
-    data.writelines(result)
+    print(*result, file=data, sep=' + ', end=' = 0\n')
